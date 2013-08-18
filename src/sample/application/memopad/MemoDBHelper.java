8940a88b118e00edaad38d07ffb3ca75859e0824
28kayak <kaya_ota@msn.com>
2013-08-18 10:50:28 -0700

    add const

--- src/sample/application/memopad/MemoDBHelper.java
+++ src/sample/application/memopad/MemoDBHelper.java
@@ -1,8 +1,18 @@
 package sample.application.memopad;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MemoDBHelper extends SQLiteOpenHelper
 {
+	public MemoDBHelper(Context context, String name, CursorFactory factory, int version)
+	{
+		super(context, name, factory, version);
+	}
+	public MemoDBHelper(Context context)
+	{
+		super(context,name,factory,version);
+	}
 
 }
