2cabe866ebef48945edb98512a8248c3b98f8779
28kayak <kaya_ota@msn.com>
2013-08-19 00:14:22 -0700

    edit

--- src/sample/application/memopad/MemoDBHelper.java
+++ src/sample/application/memopad/MemoDBHelper.java
@@ -1,18 +1,36 @@
 package sample.application.memopad;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class MemoDBHelper extends SQLiteOpenHelper
 {
-	public MemoDBHelper(Context context, String name, CursorFactory factory, int version)
-	{
-		super(context, name, factory, version);
-	}
+	static final String name = "memo.db";
+	static final int version = 1;
+	static final CursorFactory factory = null;
+	
 	public MemoDBHelper(Context context)
 	{
 		super(context,name,factory,version);
 	}
 
+	@Override
+	public void onCreate(SQLiteDatabase db)
+	{
+		String sql = "CREATE TABLE memoDB(" 
+				+ android.provider.BaseColumns._ID
+				+ "INTEGER PRIMARY KEY AUTOINCREMENT,title Text, memo Text);";
+		db.execSQL(sql);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
 }
+	
--- src/sample/application/memopad/MemopadActivity.java
+++ src/sample/application/memopad/MemopadActivity.java
@@ -11,6 +11,11 @@ import android.text.Selection;
 //a cursor is a selection where the start and end are at the same offset. 
 import android.widget.EditText;
 //EditText is a thin veneer over TextView that configures itself to be editable.
+//imports to save memo-data on data base
+import java.text.DateFormat;
+import java.util.Date;
+import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
 
 
 
@@ -55,6 +60,42 @@ public class MemopadActivity extends Activity
 		editor.putInt("cursor",Selection.getSelectionStart(et.getText()));//store current place of cursor.
 		editor.commit();	
 	}//onStop()
+	
+	void saveMemo()
+	{
+		EditText et = (EditText)this.findViewById(R.id.editText1);
+		String title;
+		String memo = et.getText().toString();//store memo_data as a string,memo.
+		
+		if(memo.trim().length() > 0)
+		{//get length of memo. save data if length of memo is more then 0
+			if(memo.indexOf("\n") == -1)
+			{
+				title = memo.substring(0, Math.min(memo.length(), 20));
+				//set title using memo-data upto 20 words. 
+				//to avoid error when memo-data is less than 20 letters,
+				//compare memo.length() and 20 and use smaller value. 
+			}
+			else
+			{
+				title = memo.substring(0, Math.min(memo.indexOf("\n"), 20));
+				//use memo-data as a title before "\n(next line)" if next line comes before 20 letters
+			}
+			String ts = DateFormat.getDateTimeInstance().format(new Date());
+			//get data and store as a string
+			MemoDBHelper memos = new MemoDBHelper(this);
+			//create an instance of MemoDBHelper
+			SQLiteDatabase db = memos.getWritableDatabase();
+			//open database with writable condition
+			ContentValues values = new ContentValues();
+			values.put("title", title + "\n" + ts); 
+			//put title on database
+			values.put("memo", memo);
+			//put memo-data on database
+			db.insertOrThrow("memoDB", null, values);
+			memos.close();//exit from MemoDBHelper
+		}
+	}
 	
 	
     
