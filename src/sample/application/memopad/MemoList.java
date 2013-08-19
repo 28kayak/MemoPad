17eb11ab407c6b167133dd368e786bc1faed682f
28kayak <kaya_ota@msn.com>
2013-08-19 01:06:09 -0700

    edit MemoList.java

--- src/sample/application/memopad/MemoList.java
+++ src/sample/application/memopad/MemoList.java
@@ -2,6 +2,7 @@ package sample.application.memopad;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.View;
 import android.widget.ListView;
 //added imports 
@@ -15,10 +16,22 @@ public class MemoList extends ListActivity
 	//cols are data of filed to be gotten by searching database
 	MemoDBHelper memos;
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) 
 	{
 		super.onListItemClick(l, v, position, id);
+		memos = new MemoDBHelper(this);
+		SQLiteDatabase db = memos.getReadableDatabase();
+		Cursor cursor = db.query("memoDB", cols, "_ID="+String.valueOf(id), null, null, null, null);
+		startManagingCursor(cursor);
+		int idx = cursor.getColumnIndex("memo");
+		cursor.moveToFirst();
+		Intent intent = new Intent();
+		intent.putExtra("text", cursor.getString(idx));
+		setResult(RESULT_OK,intent);
+		memos.close();
+		finish();
 	}
 
 	@Override
@@ -28,5 +41,25 @@ public class MemoList extends ListActivity
 		setContentView(R.layout.memolist);
 		showMemos(getMemos());
 	}
-
+	private Cursor getMemos()
+	{
+		memos = new MemoDBHelper(this);
+		SQLiteDatabase db = memos.getReadableDatabase();
+		Cursor cursor = db.query("memoDB", cols, null, null, null, null, null);
+		startManagingCursor(cursor);
+		return cursor;
+	}
+	private void showMemos(Cursor cursor)
+	{
+		if(cursor != null)
+		{
+			String [] from = {"title"};
+			int[] to = {android.R.id.text1};
+			@SuppressWarnings("deprecation")
+			SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+					android.R.layout.simple_list_item_1, cursor, from, to);
+			setListAdapter(adapter);
+		}
+		memos.close();
+	}
 }
